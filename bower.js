d9e14652a1bf4b5308b16f58c7c410c871523037
高云 <nuysoft@gmail.com>
2014-09-15 19:02:04 +0800

    + bower.js

---
+++ bower.js
@@ -0,0 +1,13 @@
+{
+  "name": "brix-loader",
+  "version": "0.0.0",
+  "homepage": "https://github.com/thx/brix-loader",
+  "license": "MIT",
+  "ignore": [
+    "**/.*",
+    "node_modules",
+    "bower_components",
+    "test"
+  ],
+  "devDependencies": {}
+}
