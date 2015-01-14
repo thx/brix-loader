45aba80365e5ead9baa3b54314681dc2361a4ab5
墨智 <mozhi.gyy@alibaba-inc.com>
2015-01-14 15:53:23 +0800

    Loader.query() 记录方法的返回值

--- dist/loader.js
+++ dist/loader.js
@@ -1386,12 +1386,13 @@ define(
             // 2. 绑定组件方法至 query() 返回的对象上
             Util.each(Util.unique(methods), function(name /*, index*/ ) {
                 results[name] = function() {
-                    // var that = this
+                    var that = this
                     var args = [].slice.call(arguments)
+                    var result
                     Util.each(this, function(instance /*, index*/ ) {
                         if (!instance[name]) return
-                            // that[index] = 
-                        instance[name].apply(instance, args)
+                        result = instance[name].apply(instance, args)
+                        if (result !== undefined) that[index] = result
                     })
                     return this
                 }
